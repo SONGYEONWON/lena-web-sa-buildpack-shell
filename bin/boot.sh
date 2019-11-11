55899f8d6270478c605cf60c39a4ca6bb433b4ec
SONGYEONWON <53226207+SONGYEONWON@users.noreply.github.com>
2019-11-11 18:10:57 +0900

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,7 +2,8 @@ export APACHE_ROOT=/home/vcap/app/httpd
 
 mv $APACHE_ROOT/env.sh $APACHE_ROOT/env.sh.org
 erb $APACHE_ROOT/env.sh.org > $APACHE_ROOT/env.sh
-(tail $APACHE_ROOT/logs/*log* &)
+#(tail $APACHE_ROOT/logs/*log &)
 #exec $APACHE_ROOT/bin/httpd -DFOREGROUND
 sh $APACHE_ROOT/start.sh
+
 echo "---------> web server start completed ->>>>>>>>>>>>>>>"
