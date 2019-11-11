f7ebdb04f11e59ca9fd7567904f64ccf77b39394
SONGYEONWON <53226207+SONGYEONWON@users.noreply.github.com>
2019-11-11 19:57:00 +0900

    Update start.sh

--- binary/httpd/start.sh
+++ binary/httpd/start.sh
@@ -86,7 +86,7 @@ if [ ${RUNNER} = ${RUN_USER} ] || [ ${RUNNER} = root ]; then
   else
     echo "" > ${INSTALL_PATH}/conf/extra/httpd-monitoring-service-port.conf
   fi
-  ${ENGN_HOME}/bin/apachectl -f ${INSTALL_PATH}/conf/httpd.conf -k start -D${MPM_TYPE} ${EXT_MODULE_DEFINES}& cat $LOG_HOME/*
+  ${ENGN_HOME}/bin/apachectl -f ${INSTALL_PATH}/conf/httpd.conf -k start -D${MPM_TYPE} ${EXT_MODULE_DEFINES}& tail -f $LOG_HOME/*
   check_web_process $?
 else
    >&2 echo "Deny Access : [ ${RUNNER} ]. Not ${RUN_USER}" ;
